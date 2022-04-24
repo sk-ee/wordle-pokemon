757e965629555cc2b7f30136a8d35f50a779c249
s-kee <73348737+sk-ee@users.noreply.github.com>
2022-04-24 21:20:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,189 @@
+var donot = false
+var isword = false
+document.addEventListener("DOMContentLoaded", () => {
+  function getCurrentWordArr() {
+    const numberOfGuessedWords = guessedWords.length;
+    return guessedWords[numberOfGuessedWords - 1];
+  }
+
+
+
+  function updateGuessedWords(letter) {
+    const currentWordArr = getCurrentWordArr();
+
+    if (currentWordArr && currentWordArr.length < 5) {
+      currentWordArr.push(letter);
+
+      const availableSpaceEl = document.getElementById(String(availableSpace));
+
+      availableSpace = availableSpace + 1;
+      availableSpaceEl.textContent = letter;
+    }
+  }
+  const keys = document.querySelectorAll(".keyboard-row button");
+  function createSquares() {
+    const gameBoard = document.getElementById("board");
+
+    for (let index = 0; index < 30; index++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add("animate__animated");
+      square.setAttribute("id", index + 1);
+      gameBoard.appendChild(square);
+    }
+  }
+
+  createSquares();
+  getNewWord();
+
+  let guessedWords = [[]];
+  let availableSpace = 1;
+
+  let word;
+  let guessedWordCount = 0;
+
+
+  function getNewWord() {
+
+    var lastscore = 0
+
+    fetch(
+     "https://raw.githubusercontent.com/sindresorhus/pokemon/main/data/en.json"
+    )
+    .then((response2 => response2.json()))
+    .then((responseJSON2 => {
+      console.log(responseJSON2.length)
+      for (var i =0; i < responseJSON2.length; i++) {
+        if (responseJSON2[i].length == 5) {
+          console.log(Math.floor(Math.random() * 12) +1 )  
+          if(Math.floor(Math.random() * 12) +1 == 10) {
+            console.log( responseJSON2[i])
+            word = responseJSON2[i].toString().toLowerCase()
+          }
+        }
+      }
+  }))
+
+ 
+
+
+  function getTileColor(letter, index) {
+    const isCorrectLetter = word.includes(letter);
+
+    if (!isCorrectLetter) {
+      return "rgb(58, 58, 60)";
+    }
+    const letterInThatPosition = word.charAt(index);
+    const isCorrectPosition = letter === letterInThatPosition;
+
+    if (isCorrectPosition) {
+      return "rgb(83, 141, 78)";
+    }
+
+    return "rgb(181, 159, 59)";
+  }
+
+  function handleSubmitWord() {
+    var currentWordArr = getCurrentWordArr();
+    var currentWord = currentWordArr.join("");
+    
+    console.log(currentWord)
+
+
+    if (guessedWords.length === 6) {
+      //window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
+    }
+
+     fetch(
+      `https://raw.githubusercontent.com/sindresorhus/pokemon/main/data/en.json`
+    )
+      .then((response => response.json()))
+      .then((responseJSON) => {
+
+        console.log(currentWord)
+        //return response.json();
+
+        for (var i =0; i < responseJSON.length; i++) {
+            if (responseJSON[i].toString().toLowerCase() === currentWord) {
+              guessedWords.push([]);
+              isword = true
+              guessedWords = guessedWords
+
+      const firstLetterId = guessedWordCount * 5 + 1;
+      const interval = 200;
+      const neword = currentWord
+      currentWordArr.forEach((letter, index) => {
+        setTimeout(() => {
+          const tileColor = getTileColor(letter, index);
+
+          const letterId = firstLetterId + index;
+          const letterEl = document.getElementById(letterId);
+          letterEl.classList.add("animate__flipInX");
+          letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+        }, interval * index);
+      });
+      setTimeout(() => {
+        if (neword === word) {
+          document.getElementById("keyboard-container").style = "pointer-events: none;"
+        }
+      }, 1000);
+
+
+      guessedWordCount += 1;
+
+            } else {
+              if (isword === true) {
+                isword = true
+
+              } else {
+                isword = false
+              }
+            }
+          }
+          if(isword === true) {
+            guessedWords.push([]);
+          }
+          if (isword === false) {
+            //window.alert("Word does not exist in our dictionary.")
+          }
+        });
+    console.log(isword)
+    console.log(currentWord)
+    if (isword === true) {
+    }
+  }
+
+  
+
+  function handleDeleteLetter() {
+    const currentWordArr = getCurrentWordArr();
+    const removedLetter = currentWordArr.pop();
+
+    guessedWords[guessedWords.length - 1] = currentWordArr;
+
+    const lastLetterEl = document.getElementById(String(availableSpace - 1));
+
+      lastLetterEl.textContent = "";
+      availableSpace = availableSpace - 1;
+
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick = ({ target }) => {
+      const letter = target.getAttribute("data-key");
+
+      if (letter === "enter") {
+        handleSubmitWord();
+        return;
+      }
+
+      if (letter === "del") {
+        handleDeleteLetter();
+        return;
+      }
+
+      updateGuessedWords(letter);
+    }
+  }
+}
+})
